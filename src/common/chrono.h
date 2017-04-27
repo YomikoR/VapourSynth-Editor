8ea1054795a3b68f57ff1c97898be783714a96d0
Mystery Keeper <mystkeeper@gmail.com>
2017-04-27 14:45:56 +0300

    Small fix in chrono header.

--- src/common/chrono.h
+++ src/common/chrono.h
@@ -5,11 +5,11 @@
 
 typedef std::chrono::high_resolution_clock::time_point hr_time_point;
 typedef std::chrono::high_resolution_clock hr_clock;
-typedef std::chrono::duration<double> double_duration;
+typedef std::chrono::duration<double, std::ratio<1, 1> > double_duration;
 
 template <typename T> double duration_to_double(const T & a_duration)
 {
-    return std::chrono::duration_cast<double_duration>(a_duration).count();
+	return std::chrono::duration_cast<double_duration>(a_duration).count();
 }
 
 #endif // CHRONO_H_INCLUDED
