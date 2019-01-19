c5f4b8faa4799d42e62fbc26556fa416ee338b03
Mystery Keeper <mystkeeper@gmail.com>
2019-01-19 20:10:44 +0300

    Update build script.

--- pro/build-msvc.bat
+++ pro/build-msvc.bat
@@ -4,7 +4,7 @@ CD %~dp0
 :: MSVC 64 bit build
 @ECHO === SETTING UP ENVIRONMENT ===
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-@SET PATH=%PATH%;S:\sdk\qt\5.11.1\msvc2017_64\bin\
+@SET PATH=%PATH%;S:\sdk\qt\5.12.0\msvc2017_64\bin\
 @ECHO === CLEANING UP BEFORE BUILDING ===
 nmake.exe distclean -f Makefile
 @ECHO === RUNNING QMAKE === 
