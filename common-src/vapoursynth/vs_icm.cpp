2a3362769f9044c95685bd3cbbaebbe2b8703310
YomikoR <monommino@gmail.com>
2022-06-13 13:16:00 +0800

    Add BT.1886 EOTF

--- common-src/vapoursynth/vs_icm.cpp
+++ common-src/vapoursynth/vs_icm.cpp
@@ -1,6 +1,8 @@
 #include "vs_icm.h"
 #include "../libp2p/p2p_api.h"
 
+#include <cmath>
+
 /* THE FORMATS
  * libp2p   p2p_abgr64
  * lcms2    TYPE_RGBA_16
@@ -10,6 +12,56 @@
 
 #define CMS_NO_REGISTER_KEYWORD 1
 #include <lcms2.h>
+
+struct cspData
+{
+    cmsFloat64Number xw, yw, xr, yr, xg, yg, xb, yb;
+};
+
+static constexpr cspData csp_709 = {0.3127, 0.3290, 0.64, 0.33, 0.3, 0.6, 0.15, 0.06};
+
+// 170m
+static constexpr cspData csp_601_525 = {0.3127, 0.3290, 0.63, 0.34, 0.31, 0.595, 0.155, 0.07};
+
+// 2020-10
+static constexpr cspData csp_2020 = {0.3127, 0.3290, 0.708, 0.292, 0.17, 0.797, 0.131, 0.046};
+
+// NTSC
+static constexpr cspData csp_1953 = {0.31, 0.316, 0.67, 0.33, 0.21, 0.71, 0.14, 0.08};
+
+cmsHPROFILE gen_bt1886_profile(const cmsHPROFILE profile, const cspData &csp)
+{
+    cmsContext ctx = cmsCreateContext(nullptr, nullptr);
+
+    // Find black pt
+    cmsCIEXYZ bp;
+    if(!cmsDetectDestinationBlackPoint(&bp, profile, INTENT_RELATIVE_COLORIMETRIC, 0))
+        return nullptr;
+    double min_luma = bp.Y; // Should be sufficient for sane usage
+
+    // Build tone curve
+    double binv = std::pow(min_luma, 1.0 / 2.4);
+    cmsFloat64Number params[4] = {2.4, 1.0 - binv, binv, 0.0};
+    cmsToneCurve *tone_curve[3];
+    if(!(tone_curve[0] = cmsBuildParametricToneCurve(ctx, 6, params)))
+        return nullptr;
+    tone_curve[1] = tone_curve[0];
+    tone_curve[2] = tone_curve[0];
+
+    // Create profile
+    cmsCIExyY wp_xyY = {csp.xw, csp.yw, 1.0};
+    cmsCIExyYTRIPLE prim_xyY = {
+        {csp.xr, csp.yr, 1.0},
+        {csp.xg, csp.yg, 1.0},
+        {csp.xb, csp.yb, 1.0}
+    };
+    cmsHPROFILE profile_dst = cmsCreateRGBProfile(&wp_xyY, &prim_xyY, tone_curve);
+    cmsFreeToneCurve(tone_curve[0]);
+
+    cmsDeleteContext(ctx);
+
+    return profile_dst;
+}
 
 struct icmData
 {
@@ -107,11 +159,18 @@ void VS_CC icmCreate(const VSMap *in, VSMap *out, VSCore *core, const VSAPI *vsa
         return;
     }
 
-    cmsHPROFILE icm_dst = cmsCreate_sRGBProfile();
+    cmsHPROFILE icm_dst = gen_bt1886_profile(icm, csp_709);
+    if(!icm_dst)
+    {
+        vsapi->freeNode(d.node);
+        vsapi->setError(out, "icm: Unable to create BT.1886 profile.");
+        cmsCloseProfile(icm);
+        return;
+    }
 
-    cmsUInt32Number intent = cmsGetHeaderRenderingIntent(icm);
+    cmsUInt32Number intent = INTENT_RELATIVE_COLORIMETRIC;
 
-    cmsUInt32Number flags = cmsFLAGS_HIGHRESPRECALC;
+    cmsUInt32Number flags = cmsFLAGS_HIGHRESPRECALC | cmsFLAGS_BLACKPOINTCOMPENSATION;
 
     d.transform = cmsCreateTransform(icm_dst, icm_datatype, icm, icm_datatype, intent, flags);
     if (!d.transform)
