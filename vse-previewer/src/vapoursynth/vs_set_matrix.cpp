f6bcd4c342c8036c1375ff51fa20ceaba16d9055
YomikoR <monommino@gmail.com>
2024-03-27 09:30:57 +0800

    fix setting _Matrix regression

--- vse-previewer/src/vapoursynth/vs_set_matrix.cpp
+++ vse-previewer/src/vapoursynth/vs_set_matrix.cpp
@@ -3,43 +3,85 @@
 #include <vapoursynth/VSHelper4.h>
 #include <vapoursynth/VSConstants4.h>
 
-const VSFrame * VS_CC setMatrixGetFrame(int n, int activationReason, void *instanceData, [[maybe_unused]] void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi)
+#include <memory>
+
+struct setMatrixData
 {
-    VSNode *node = reinterpret_cast<VSNode *>(instanceData);
+    VSNode *node;
+    VSVideoInfo vi;
+    int64_t matrix_in;
+};
+
+const VSFrame * VS_CC setMatrixGetFrame(int n, int activationReason, void *instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi)
+{
+    setMatrixData *d = reinterpret_cast<setMatrixData *>(instanceData);
     if (activationReason == arInitial)
     {
-        vsapi->requestFrameFilter(n, node, frameCtx);
+        vsapi->requestFrameFilter(n, d->node, frameCtx);
     }
     else if (activationReason == arAllFramesReady)
     {
-        const VSFrame *src_frame = vsapi->getFrameFilter(n, node, frameCtx);
+        const VSFrame *src_frame = vsapi->getFrameFilter(n, d->node, frameCtx);
+
+        /* RGB: tag RGB matrix
+           YUV: if missing matrix, add it
+           GRAY: if having RGB matrix, drop it
+         */
+
         const VSVideoFormat *vff = vsapi->getVideoFrameFormat(src_frame);
-        if (vff->colorFamily == cfGray)
+
+        VSFrame *dst_frame = vsapi->copyFrame(src_frame, core);
+        vsapi->freeFrame(src_frame);
+        VSMap *props = vsapi->getFramePropertiesRW(dst_frame);
+
+        if (vff->colorFamily == cfRGB)
         {
-            const VSMap *src_props = vsapi->getFramePropertiesRO(src_frame);
+            vsapi->mapSetInt(props, "_Matrix", VSC_MATRIX_RGB, maReplace);
+        }
+        else if (vff->colorFamily == cfYUV)
+        {
             int err;
-            int64_t matrix = vsapi->mapGetInt(src_props, "_Matrix", 0, &err);
-            if (!err && matrix == VSC_MATRIX_RGB)
+            int64_t matrix = vsapi->mapGetInt(props, "_Matrix", 0, &err);
+            if (err)
             {
-                VSFrame *dst_frame = vsapi->copyFrame(src_frame, core);
-                vsapi->freeFrame(src_frame);
-                vsapi->mapDeleteKey(vsapi->getFramePropertiesRW(dst_frame), "_Matrix");
-                return dst_frame;
+                vsapi->mapSetInt(props, "_Matrix", d->matrix_in, maReplace);
             }
         }
-        return src_frame;
+        else
+        {
+            int err;
+            int64_t matrix = vsapi->mapGetInt(props, "_Matrix", 0, &err);
+            if (!err && matrix == VSC_MATRIX_RGB)
+            {
+                vsapi->mapDeleteKey(props, "_Matrix");
+            }
+        }
+        return dst_frame;
     }
     return nullptr;
 }
 
-void VS_CC setMatrixFree(void *instanceData, [[maybe_unused]] VSCore *core, const VSAPI *vsapi)
+void VS_CC setMatrixFree(void *instanceData, VSCore *core, const VSAPI *vsapi)
 {
-    vsapi->freeNode(reinterpret_cast<VSNode *>(instanceData));
+    setMatrixData *d = reinterpret_cast<setMatrixData *>(instanceData);
+    vsapi->freeNode(d->node);
+    delete d;
 }
 
 void VS_CC setMatrixFilter(const VSMap *in, VSMap *out, VSCore *core, const VSAPI *vsapi)
 {
-    VSNode *node = vsapi->mapGetNode(in, "clip", 0, nullptr);
-    VSFilterDependency deps[] = {{node, rpStrictSpatial}};
-    vsapi->createVideoFilter(out, "SetMatrix", vsapi->getVideoInfo(node), setMatrixGetFrame, setMatrixFree, fmParallel, deps, 1, node, core);
+    std::unique_ptr<setMatrixData> d(new setMatrixData());
+
+    d->node = vsapi->mapGetNode(in, "clip", 0, nullptr);
+    const VSVideoInfo *vi = vsapi->getVideoInfo(d->node);
+    d->vi = *vi;
+
+    int err;
+    d->matrix_in = vsapi->mapGetInt(in, "matrix_in", 0, &err);
+
+    VSFilterDependency deps[] = {{d->node, rpStrictSpatial}};
+
+    vsapi->createVideoFilter(out, "SetMatrix", &d->vi, setMatrixGetFrame, setMatrixFree, fmParallel, deps, 1, d.get(), core);
+
+    d.release();
 }
