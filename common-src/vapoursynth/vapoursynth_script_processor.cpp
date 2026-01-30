78c038442ba6793c4b3bbd86b087f532636a9859
YomikoR <64504948+YomikoR@users.noreply.github.com>
2026-01-30 15:37:16 +0800

    Adapt to VS API 4.2 changes Support reserved frame prop _Range

--- common-src/vapoursynth/vapoursynth_script_processor.cpp
+++ common-src/vapoursynth/vapoursynth_script_processor.cpp
@@ -1016,6 +1016,7 @@ QString VapourSynthScriptProcessor::framePropsString(
 	static std::map<QString, std::map<int64_t, QString>> reservedPropToMap =
 	{
 		{"_ChromaLocation", _ChromaLocationToString},
+		{"_Range", _ColorRangeToString},
 		{"_ColorRange", _ColorRangeToString},
 		{"_Primaries", _PrimariesToString},
 		{"_Matrix", _MatrixToString},
